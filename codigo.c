ed763c64b74cd6f60838aee51dbe6f07dfb7aee3
Joao Victor <joaovictorgi13@gmail.com>
2025-11-18 13:09:56 -0300

    Adicionando comentarios no codigo

--- codigo.c
+++ codigo.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-
+ //Estrutura para representar um produto, com suas respectivas caracteristicas(nome, preço, quantidade).
 typedef struct {
     char Name[50];
     float Price;
     int Quantity;
 }Product;
 
-
+//Estrutura para representar um pedido(ID do pedido, Array de até 99 produtos, Soma do valor total do pedido).
 typedef struct {
     int Id;
     Product Products[99];
@@ -16,11 +16,12 @@ typedef struct {
 }Order;
 
 int main() {
-    Order orders[99];
+    Order orders[99]; // Vetor de 99 pedidos.
     int id = 0, status = 1, c;
 
+    // loop que controla todo o programa.
     while (status) {
-        printf("\e[1;1H\e[2J");
+        printf("\e[1;1H\e[2J"); // Limpa o terminal.
         printf("1 - New order");
         printf("\n2 - List order(s)");
         printf("\n3 - Exit");
@@ -37,6 +38,7 @@ int main() {
                 orders[id].Id = id;
                 float total = 0;
 
+                // loop para adicionar produtos
                 for (int i = 0; i < 99; i++) {
                     while ((c = getchar()) != '\n' && c != EOF);
 
@@ -44,7 +46,7 @@ int main() {
                 
                     printf("\nEnter the name of the product: ");    
                     char name[50];
-                    fgets(name, sizeof(name), stdin);
+                    fgets(name, sizeof(name), stdin); // atribui o nome do produto a variavel
 
                     printf("\nEnter the price of the product: ");
                     float price;
@@ -54,12 +56,14 @@ int main() {
                     int quantity;
                     scanf("%d", &quantity);
                     
+                    // cria produto com os dados informados
                     Product product = {.Price = price, .Quantity = quantity};
-                    strcpy(product.Name, name);
+                    strcpy(product.Name, name); //copia o nome para a struct
                 
-                    orders[id].Products[i] = product;
+                    orders[id].Products[i] = product; //Salva no pedido.
 
-                    total += price * quantity;
+
+                    total += price * quantity; // soma o produto adicionado ao total.
 
                     printf("\nDo you want to add another product? [y/n] ");
                     char newProduct;
@@ -75,11 +79,12 @@ int main() {
                 orders[id].Total = total;
                 id++;
                 break;
-            case 2:
+
+            case 2: //listar pedidos
                 printf("\e[1;1H\e[2J");
                 float overallTotal = 0;
 
-                
+                // percorre todos os pedidos que ja foram criados.
                 for (int i = 0; i < id; i++) {
                     if (i > 0) {
                         printf("\n");
@@ -91,7 +96,8 @@ int main() {
                         printf("\nProduct's name: %s", orders[i].Products[j].Name);
                         printf("Product's price: %f", orders[i].Products[j].Price);
                         printf("\nProduct's quantity: %d", orders[i].Products[j].Quantity);
-
+                        
+                        // se o próximo produto tem quantidade 0, significa que não existe
                         if (orders[i].Products[j + 1].Quantity == 0) {
                             break;
                         }
@@ -103,10 +109,11 @@ int main() {
                 }
                 printf("\n- Overall total: %f\n", overallTotal);
                 printf("\nPress any key to continue...");
+                // aguarda botão para continuar
                 while ((c = getchar()) != '\n' && c != EOF);
                 getchar();
                 break;
-            case 3:
+            case 3: // fechar o programa
                 status = 0;
                 break;
         }
